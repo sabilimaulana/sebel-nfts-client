422027efc8ca8f7ec54f229ad686e99488c7bcdd
sabilimaulana <spacysbl.learn@gmail.com>
2022-05-21 18:47:08 +0700

    done: Section 4

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import myEpicNft from "./utils/myEpicNft.json";
 
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const BILI_TWITTER_HANDLE = "sabilimawlana";
+const BILI_TWITTER_LINK = `https://twitter.com/${BILI_TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
@@ -13,7 +15,7 @@ const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x4a58b87F290F2249985EBEB6C8DcD0153d41f00d";
+    const CONTRACT_ADDRESS = "0x01BfD74DB4574B14bCC544d36334BF8619859129";
 
     try {
       const { ethereum } = window;
@@ -63,11 +65,52 @@ const App = () => {
     } else {
       console.log("No authorized account found");
     }
+
+    let chainId = await ethereum.request({ method: "eth_chainId" });
+    console.log("Connected to chain " + chainId);
+
+    // String, hex code of the chainId of the Rinkebey test network
+    const rinkebyChainId = "0x4";
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
   };
 
-  /*
-   * Implement your connectWallet method here
-   */
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -77,18 +120,16 @@ const App = () => {
         return;
       }
 
-      /*
-       * Fancy method to request access to account.
-       */
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
 
-      /*
-       * Boom! This should print out public address once we authorize Metamask.
-       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
     } catch (error) {
       console.log(error);
     }
@@ -138,6 +179,13 @@ const App = () => {
             target="_blank"
             rel="noreferrer"
           >{`built on @${TWITTER_HANDLE}`}</a>
+          <span>, </span>
+          <a
+            className="footer-text"
+            href={BILI_TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`by @${BILI_TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
